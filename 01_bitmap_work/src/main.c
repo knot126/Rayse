bc067dc4d2a50602b0d21e32a12c2501169ab9d6
knot126 <73200930+knot126@users.noreply.github.com>
2022-03-05 21:43:20 +0000

    Test program

--- 01_bitmap_work/src/main.c
+++ 01_bitmap_work/src/main.c
@@ -12,6 +12,92 @@
 
 #include "common.h"
 
+void KI_Convex_Polygon_Test(DgBitmap *bmp) {
+	for (size_t i = 0; i < 25; i++) {
+		DgVec2 points[] = {
+			{DgRandFloat(), DgRandFloat()},
+			{DgRandFloat(), DgRandFloat()},
+			{DgRandFloat(), DgRandFloat()}
+		};
+		DgBitmapDrawConvexPolygon(bmp, 3, points, &(DgVec4) {DgRandFloat(), DgRandFloat(), DgRandFloat(), 0.5f + 0.5f * DgRandFloat()});
+	}
+}
+
+void KI_Triangle_Test(DgBitmap *bmp) {
+	DgBitmapTriangle t[] = {
+		(DgBitmapTriangle) {
+			(DgBitmapVertex) {
+				(DgVec3) {-1.0f, 1.0f, 2.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {1.0f, 0.0f, 0.0f, 0.5f}
+			},
+			(DgBitmapVertex) {
+				(DgVec3) {-1.0f, -1.0f, 2.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {0.0f, 1.0f, 0.0f, 0.5f}
+			},
+			(DgBitmapVertex) {
+				(DgVec3) {1.0f, -1.0f, 2.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {0.0f, 0.0f, 1.0f, 0.5f}
+			}
+		},
+		(DgBitmapTriangle) {
+			(DgBitmapVertex) {
+				(DgVec3) {-1.0f, 1.0f, 2.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {1.0f, 0.0f, 0.0f, 0.5f}
+			},
+			(DgBitmapVertex) {
+				(DgVec3) {1.0f, 1.0f, 2.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {1.0f, 1.0f, 0.0f, 0.5f}
+			},
+			(DgBitmapVertex) {
+				(DgVec3) {1.0f, -1.0f, 2.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {0.0f, 0.0f, 1.0f, 0.5f}
+			}
+		},
+		(DgBitmapTriangle) {
+			(DgBitmapVertex) {
+				(DgVec3) {1.0f, 1.0f, 3.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {1.0f, 0.0f, 1.0f, 0.5f}
+			},
+			(DgBitmapVertex) {
+				(DgVec3) {1.0f, 1.0f, 2.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {1.0f, 1.0f, 0.0f, 0.5f}
+			},
+			(DgBitmapVertex) {
+				(DgVec3) {1.0f, -1.0f, 2.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {0.0f, 0.0f, 1.0f, 0.5f}
+			}
+		},
+		(DgBitmapTriangle) {
+			(DgBitmapVertex) {
+				(DgVec3) {1.0f, 1.0f, 3.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {1.0f, 0.0f, 1.0f, 0.5f}
+			},
+			(DgBitmapVertex) {
+				(DgVec3) {1.0f, -1.0f, 3.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {0.0f, 1.0f, 1.0f, 0.5f}
+			},
+			(DgBitmapVertex) {
+				(DgVec3) {1.0f, -1.0f, 2.0f},
+				(DgVec2) {0.0f, 0.0f},
+				(DgColour) {0.0f, 0.0f, 1.0f, 0.5f}
+			}
+		},
+	};
+	
+	DgBitmapDrawTriangles(bmp, 4, &t);
+}
+
 int main(int argc, const char *argv[]) {
 	DgLog(DG_LOG_INFO, "Hello, world!\n");
 	
@@ -24,18 +110,11 @@ int main(int argc, const char *argv[]) {
 		return 1;
 	}
 	
-	DgBitmapFill(&bmp, (DgVec4) {DgRandFloat(), DgRandFloat(), DgRandFloat(), 1.0f});
+	DgBitmapFill(&bmp, (DgVec4) {0.0f, 0.0f, 0.0f, 1.0f});
 	
-	DgBitmapSetFlags(&bmp, DG_BITMAP_DRAWING_ALPHA);
+	DgBitmapSetFlags(&bmp, DG_BITMAP_DRAWING_ALPHA | DG_BITMAP_DRAWING_PERSPECTIVE);
 	
-	for (size_t i = 0; i < 25; i++) {
-		DgVec2 points[] = {
-			{DgRandFloat(), DgRandFloat()},
-			{DgRandFloat(), DgRandFloat()},
-			{DgRandFloat(), DgRandFloat()}
-		};
-		DgBitmapDrawConvexPolygon(&bmp, 3, points, &(DgVec4) {DgRandFloat(), DgRandFloat(), DgRandFloat(), 0.5f + 0.5f * DgRandFloat()});
-	}
+	KI_Triangle_Test(&bmp);
 	
 	DgBitmapWritePPM(&bmp, "assets://image.ppm");
 	DgBitmapFree(&bmp);
